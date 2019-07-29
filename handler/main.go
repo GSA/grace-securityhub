ba8eb9309874d6dc5f556c5dbbfbe49bd20c030a
Bryan Alexander <bryan.alexander@gsa.gov>
2019-07-29 10:13:07 -0500

    adds lambda handler for securityhub findings

--- handler/main.go
+++ handler/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/GSA/grace-securityhub/handler/event"
+	"github.com/GSA/grace-securityhub/handler/types"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
+func handler(evt event.Event) error {
+	event.Register(
+
+		// Generic should be the final type registered as it always matches
+		&types.Generic{},
+	)
+	p, err := event.New()
+	if err != nil {
+		return err
+	}
+	err = p.Publish(evt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
