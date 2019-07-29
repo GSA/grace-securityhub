d6002ff0a66d13694c675f1dce547e29a7f15127
Taofiq Badmus <tbadmus1975@gmail.com>
2019-07-29 14:05:17 -0400

    update with cloudwatch

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,19 @@ module "s3" {
   config_bucket         = "grace-${module.app.env}-config"
 }
 
+module "cloudwatch" {
+  source                       = "./cloudwatch"
+  env                          = "${module.app.env}"
+  aws_account_id               = "${module.app.account_id}"
+  master_aws_account_id        = "${var.master_aws_account_id}"
+  aws_region                   = "${var.aws_region}"
+  bucket_name                  = "${module.s3.bucket_name}"
+  cloudtrail_kms_key           = "${module.s3.cloudtrail_kms_key}"
+  cloudwatch_delivery_role_id  = "${module.iam.cloudwatch_delivery_role_id}"
+  cloudwatch_delivery_role_arn = "${module.iam.cloudwatch_delivery_role_arn}"
+  email_address                = "${var.email_address}"
+}
+
 module "config" {
   source         = "./config"
   name           = "${module.s3.bucket_name}"
