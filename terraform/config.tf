df31afa90a6a6d1c261c2500ee7d7a6a3f1c8cb3
Bryan Alexander <bryan.alexander@gsa.gov>
2019-08-05 09:21:01 -0500

    removes old aws_ prefix from config.tf variable references

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "config" {
-  name = "${var.aws_config_role_name}"
+  name = "${var.config_role_name}"
 
   assume_role_policy = <<POLICY
 {
@@ -19,31 +19,31 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "config" {
-  role = "${aws_iam_role.config.name}"
+  role       = "${aws_iam_role.config.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
 resource "aws_iam_role_policy_attachment" "organization" {
-  role = "${aws_iam_role.config.name}"
+  role       = "${aws_iam_role.config.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"
 }
 
 resource "aws_config_configuration_recorder" "config" {
-  name = "${var.aws_config_recorder_name}"
+  name     = "${var.config_recorder_name}"
   role_arn = "${aws_iam_role.config.arn}"
   recording_group {
-    all_supported = "${var.aws_config_recorder_group_all_supported}"
-    include_global_resource_types = "${var.aws_config_recorder_group_include_global}"
+    all_supported                 = "${var.config_recorder_group_all_supported}"
+    include_global_resource_types = "${var.config_recorder_group_include_global}"
   }
 }
 
 resource "aws_config_delivery_channel" "config" {
-  name = "${var.aws_config_delivery_name}"
-  s3_bucket_name = "${var.aws_config_delivery_bucket}"
-  s3_key_prefix = "${var.aws_config_delivery_bucket_prefix}"
+  name           = "${var.config_delivery_name}"
+  s3_bucket_name = "${var.config_delivery_bucket}"
+  s3_key_prefix  = "${var.config_delivery_bucket_prefix}"
 
   snapshot_delivery_properties {
-    delivery_frequency = "${var.aws_config_delivery_frequency}"
+    delivery_frequency = "${var.config_delivery_frequency}"
   }
 
   depends_on = ["aws_config_configuration_recorder.config"]
@@ -52,7 +52,7 @@ resource "aws_config_delivery_channel" "config" {
 resource "aws_config_configuration_recorder_status" "config" {
   name = "${aws_config_configuration_recorder.config.name}"
 
-  is_enabled = "${var.aws_config_recorder_enabled}"
+  is_enabled = "${var.config_recorder_enabled}"
 
   depends_on = ["aws_config_delivery_channel.config"]
 }
