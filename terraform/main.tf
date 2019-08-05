798722cf570972017acfe46319f44a48ce09f233
Bryan Alexander <bryan.alexander@gsa.gov>
2019-08-05 09:14:52 -0500

    removes duplicate resource name in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
+data "aws_caller_identity" "identity" {}
+data "aws_region" "region" {}
 
 locals {
   app_name   = "grace-securityhub"
-  account_id = "${data.aws_caller_identity.current.account_id}"
-  region     = "${data.aws_region.current.name}"
+  account_id = "${data.aws_caller_identity.identity.account_id}"
+  region     = "${data.aws_region.region.name}"
 }
